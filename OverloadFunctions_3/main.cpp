11ddd0cd1407d7ac36a78f59f4db85247b7c3417
trimmergrom@yandex.ru <trimmergrom@yandex.ru>
2021-11-05 20:30:58 +0300

    Rel

--- OverloadFunctions_3/main.cpp
+++ OverloadFunctions_3/main.cpp
@@ -6,6 +6,19 @@
 #include "Shift.h"
 #define delimiter "\n===================================================================\n"
 
+void ArrRand(int arr[], int arr_1[], const unsigned int n, int minRand, int maxRand);
+void ArrRand(double arr[], double arr_1[], const unsigned int n, int minRand, int maxRand);
+void ArrRand(int arr[ROWS][COLS], int arr_1[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS, int minRand, int maxRand);
+void ArrRand(double arr[ROWS][COLS], double arr_1[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS, int minRand, int maxRand);
+
+template<typename T> void Print(T arr[n], const unsigned int n);
+template<typename T> void Print(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS);
+
+template<typename T> void ReversPrint(T arr[n], const unsigned n);
+template<typename T> void ReversPrint(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS);
+
+
+
 int main()
 {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
