388a9cb54ac751296b9e5dfc3405b2821b8b48d3
trimmergrom@yandex.ru <trimmergrom@yandex.ru>
2021-11-05 20:39:06 +0300

    Rel4

--- OverloadFunctions_3/main.cpp
+++ OverloadFunctions_3/main.cpp
@@ -14,6 +14,18 @@ template<typename T> void Print(T arr[ROWS][COLS], const unsigned int ROWS, cons
 template<typename T> void ReversPrint(T arr[n], const unsigned n);
 template<typename T> void ReversPrint(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS);
 
+template<typename T> T Summ(T arr[], const unsigned int n);
+template<typename T> T Summ(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS);
+template<typename T> T Avg(T summ, const unsigned int n);
+template<typename T> T Avg(T summ, const unsigned int ROWS, const unsigned int COLS);
+template<typename T> T MaxValue(T arr[], const unsigned int n);
+template<typename T> T MinValue(T arr[], const unsigned int n);
+template<typename T> T MaxValue(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS);
+template<typename T> T MinValue(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS);
+template<typename T> void SortArr(T arr[], const unsigned int n);
+template<typename T> void SortArr(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS);
+
+
 
 
 int main()
