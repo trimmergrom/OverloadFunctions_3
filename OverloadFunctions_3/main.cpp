d1f65e866f2023d807daebc408f4af28d7b4a51d
trimmergrom@yandex.ru <trimmergrom@yandex.ru>
2021-11-05 20:32:31 +0300

    Rel2

--- OverloadFunctions_3/main.cpp
+++ OverloadFunctions_3/main.cpp
@@ -79,3 +79,48 @@ int main()
 
 	return 0;
 }
+void ArrRand(int arr[], int arr_1[], const unsigned int n, int minRand, int maxRand)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+		arr_1[i] = arr[i];
+	}
+}
+void ArrRand(double arr[], double arr_1[], const unsigned int n, int minRand, int maxRand)
+{
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+		arr[i] /= 100;
+		arr_1[i] = arr[i];
+	}
+}
+void ArrRand(int arr[ROWS][COLS], int arr_1[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS, int minRand, int maxRand)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % (maxRand - minRand) + minRand;
+			arr_1[i][j] = arr[i][j];
+		}
+	}
+}
+void ArrRand(double arr[ROWS][COLS], double arr_1[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS, int minRand, int maxRand)
+{
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % (maxRand - minRand) + minRand;
+			arr[i][j] /= 100;
+			arr_1[i][j] = arr[i][j];
+		}
+	}
+}
+
